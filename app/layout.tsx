9eb170ae2e0f050cd0eb8a813d17a79127d50280
Samuli Reijonen <samuli.o.reijonen@gmail.com>
2024-05-17 13:20:21 +0300

    add frontpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Lolchan",
-	description: "Image board",
+	title: "Imageboard",
+	description: "Imageboard",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<>
-			<div>Hello World</div>
-			<Link href="/b">Random - /b/</Link>
+			<div className="flex justify-center align-middle h-[100vh]">
+				<div className="m-auto">
+					<h1 className="text-xl font-bold">Welcome, Traveler!</h1>
+					<p>This is an experimental imageboard created as a quick side project just for fun.</p>
+					<p>Leave your greetings for others as you pass by.</p>
+					<div className="mt-4">
+						<h2 className="text-lg font-bold">Boards</h2>
+						<ul>
+							<li><Link href="/b" className="text-blue-900">Random</Link></li>
+						</ul>
+					</div>
+				</div>
+				<Link href="https://github.com/reijonen/imageboard" target="_blank" className="absolute right-4 bottom-2 text-sm italic text-blue-900">Source Code</Link>
+			</div>
 		</>
 	);
 }
